245b57291f3a406d716814d52b1177fa9d5e4883
Kiefer <149935970+Liojn@users.noreply.github.com>
2024-04-15 14:24:05 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Question3
+public class Main
 {
   public static void main(String[] args)
   {
@@ -9,8 +9,11 @@ public class Question3
      * 
      * Hint: in.nextInt() to read integer
      */
-     
+
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter an integer: ");
+    int num = in.nextInt();
+    int numSquared = num * num;
+    System.out.print(num + " squared is " + numSquared);
   }
 }
