4e9622a04a5ef0e52ebc3f1e989a91aad7baf9d1
Kiefer <149935970+Liojn@users.noreply.github.com>
2024-04-15 15:26:28 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -16,6 +16,6 @@ public class Question2
     double weight = in2.nextDouble();
 
     double BMI = weight/(height*height);
-    System.out.printf("%.2f\n", BMI);
+    System.out.printf("%.1f\n", BMI);
   }
 }
